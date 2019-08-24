83f786abbbf0d863d63f9708a7acd846dcf55f89
quintanafabio <fabioquintana96@gmail.com>
2019-08-24 10:39:34 -0300

    primer commit

---
+++ nueva1/index.php
@@ -0,0 +1,16 @@
+<?php
+//una variable es un contenedor de informacion, puedo guardar cualquier dato dentro de ella.
+
+$mi_primera_variable = "Hola mundo desde una variable";
+$numero = 44;
+$verdadero = true;
+
+$numero = 77;
+
+echo '<h1>'.$mi_primera_variable.'</h1>';
+echo $numero."<br>";
+
+$numero = 120;
+echo $numero;
+
+?>
